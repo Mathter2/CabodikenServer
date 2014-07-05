09649cd03d49e30353389f1c4df3d6fd3148cdc3
Matias Fierro <mathter@gmail.com>
2014-07-05 13:11:15 -0300

    Created generation algorithm

--- Cabodiken Server/Domain/GameManager.vb
+++ Cabodiken Server/Domain/GameManager.vb
@@ -8,6 +8,8 @@ Namespace Domain
         Private Shared _instance As GameManager = New GameManager()
         Private _games As Dictionary(Of String, Game)
         Private _invitations As Dictionary(Of String, InvitationData)
+        Private _allowedChars As Char()
+        Private _random As Random
 
         Public Shared ReadOnly Property Instance As GameManager
             Get
@@ -20,7 +22,8 @@ Namespace Domain
 #Region "Constructors"
 
         Private Sub New()
-
+            _allowedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLOMNOPQRSTUVWXYZ0123456789".ToCharArray()
+            _random = New Random
         End Sub
 
 #End Region
@@ -31,7 +34,10 @@ Namespace Domain
 
         End Sub
 
-        Public Function CreateGameSession(sessionTokenId As String, gameId As Integer) As String
+        Public Function CreateGameSession(sessionTokenId As String, gameId As Integer, _
+                                          gameName As String) As String
+            Dim newGame As New Game
+            Dim newGameSessionId As String = GenerateGameSessionId(gameName)
 
         End Function
 
@@ -63,8 +69,66 @@ Namespace Domain
 
         End Function
 
-        Private Function GenerateGameSessionId() As String
+        Private Function GenerateGameSessionId(gameName As String) As String
+            Dim gameSessionId As String = ""
+            Dim ticks As String = Date.Now.Ticks.ToString
+            Dim random As New Random()
 
+            If gameName.Length < 6 Then
+                For i As Integer = gameName.Length To 6
+                    gameName += GetRandomAlphaNumber()
+                Next
+            End If
+
+            If ticks.Length < 7 Then
+                For i As Integer = ticks.Length To 7
+                    ticks += "0"
+                Next
+            End If
+
+            For i As Integer = 1 To 64
+                Select Case i
+                    Case 4
+                        gameSessionId &= ticks.ToCharArray.GetValue(ticks.Length - 1).ToString
+                    Case 14
+                        gameSessionId &= ticks.ToCharArray.GetValue(ticks.Length - 2).ToString
+                    Case 24
+                        gameSessionId &= ticks.ToCharArray.GetValue(ticks.Length - 3).ToString
+                    Case 34
+                        gameSessionId &= ticks.ToCharArray.GetValue(ticks.Length - 4).ToString
+                    Case 44
+                        gameSessionId &= ticks.ToCharArray.GetValue(ticks.Length - 5).ToString
+                    Case 54
+                        gameSessionId &= ticks.ToCharArray.GetValue(ticks.Length - 6).ToString
+                    Case 64
+                        gameSessionId &= ticks.ToCharArray.GetValue(ticks.Length - 7).ToString
+                    Case 10
+                        gameSessionId &= gameName.ToCharArray.GetValue(3).ToString
+                    Case 20
+                        gameSessionId &= gameName.ToCharArray.GetValue(5).ToString
+                    Case 30
+                        gameSessionId &= gameName.ToCharArray.GetValue(4).ToString
+                    Case 40
+                        gameSessionId &= gameName.ToCharArray.GetValue(0).ToString
+                    Case 50
+                        gameSessionId &= gameName.ToCharArray.GetValue(2).ToString
+                    Case 60
+                        gameSessionId &= gameName.ToCharArray.GetValue(1).ToString
+                    Case Else
+                        gameSessionId &= GetRandomAlphaNumber()
+                End Select
+            Next
+
+            If _games.ContainsKey(gameSessionId) Then
+                gameSessionId = GenerateGameSessionId(gameName)
+            End If
+
+            Return gameSessionId
+
+        End Function
+
+        Private Function GetRandomAlphaNumber() As String
+            Return _allowedChars(_random.Next(_allowedChars.Length - 1))
         End Function
 
 #End Region
