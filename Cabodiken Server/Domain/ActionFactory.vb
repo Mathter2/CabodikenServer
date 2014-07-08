2e1c0d0e70b85ae1f39fb016a092c5dbe1cc5dd6
Matias Fierro <mathter@gmail.com>
2014-07-08 12:20:08 -0300

    Implemented ActionFactory

--- Cabodiken Server/Domain/ActionFactory.vb
+++ Cabodiken Server/Domain/ActionFactory.vb
@@ -1,5 +1,20 @@
 ﻿Namespace Domain
     Public Class ActionFactory
 
+        Public Function GetAction(actionName As String) As IAction
+
+            Select Case actionName.ToUpper
+                Case "MOVE", "ROTATE", "LOCK"
+                    Return New BasicAction()
+                Case "SHUFFLE", "AGGREGATE", "SPLIT", "DRAW"
+                    Return New ContainerAction()
+                Case "FLIP", "SPIN", "THROW"
+                    Return New SpecialAction()
+                Case Else
+                    Return New CustomAction()
+            End Select
+
+        End Function
+
     End Class
 End Namespace
