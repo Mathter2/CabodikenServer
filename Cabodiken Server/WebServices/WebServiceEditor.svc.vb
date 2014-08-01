84e84858c6888ab2e3392563ed479ae4d1499bd5
Matias Fierro <mathter@gmail.com>
2014-08-01 15:47:09 -0300

    Added a web service for the resource editor

---
+++ Cabodiken Server/WebServices/WebServiceEditor.svc.vb
@@ -0,0 +1,14 @@
+﻿Namespace WebServices
+
+    Public Class WebServiceEditor
+        Implements IWebServiceEditor
+
+        Public Function CreateDeckV01(newDeck As DataObjects.DeckData) _
+                As Boolean Implements IWebServiceEditor.CreateDeckV01
+
+
+
+        End Function
+    End Class
+
+End Namespace
